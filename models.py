d93f982485b35641861461ebc3cde2c37cdb93c7
Luispmv <luispmv07@hotmail.com>
2025-09-06 19:15:57 -0600

    Creando nuestra primer tabla en sqlite a traves del modelo Usuario

--- models.py
+++ models.py
@@ -1,34 +1,35 @@
 from pydantic import BaseModel
+from sqlmodel import SQLModel, Field
 
 # Datos del usuario que necesitamos para rellenar en formularios
-class UsuarioBase(BaseModel):
-    correo: str
-    password: str
-    nombre: str
-    apellido: str
-    fecha_nacimiento: str
-    peso: str
-    altura: str
-    foto_perfil: str
-    objetivo: str
+class UsuarioBase(SQLModel):
+    correo: str = Field(default=None, max_length=50, nullable=False)
+    password: str = Field(default=None, max_length=50, nullable=False)
+    nombre: str = Field(default=None, max_length=50, nullable=False)
+    apellido: str = Field(default=None, max_length=50, nullable=False)
+    fecha_nacimiento: str = Field(default=None, max_length=50, nullable=False)
+    peso: str = Field(default=None, max_length=50, nullable=False)
+    altura: str = Field(default=None, max_length=50, nullable=False)
+    foto_perfil: str = Field(default=None, max_length=50, nullable=False)
+    objetivo: str = Field(default=None, max_length=50, nullable=False)
 
 # Generacion de id de manera automatica
-class Usuario(UsuarioBase):
-    id_usuario:int | None = None
+class Usuario(UsuarioBase, table=True):
+    id_usuario:int | None = Field(default=None, primary_key=True)
 
 
-class Rutina(BaseModel):
-    id_rutina: int
-    id_nivel_dificultad: int
-    nombre: str
-    descripcion: str
-    fecha_creacion: str
-    duracion: int
-    frecuencia_semanal: int 
+# class Rutina(BaseModel):
+#     id_rutina: int
+#     id_nivel_dificultad: int
+#     nombre: str
+#     descripcion: str
+#     fecha_creacion: str
+#     duracion: int
+#     frecuencia_semanal: int 
 
-class Ejercicio(BaseModel):
-    id_ejercicio: int
-    id_nivel_dificultad: int
-    nombre: str
-    descripcion: str
-    video_demostrativo: str
+# class Ejercicio(BaseModel):
+#     id_ejercicio: int
+#     id_nivel_dificultad: int
+#     nombre: str
+#     descripcion: str
+#     video_demostrativo: str
