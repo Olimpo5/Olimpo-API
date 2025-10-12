6c76a65cac43e0f7f55256ebeb8c1047aecd55d8
Luispmv <luispmv07@hotmail.com>
2025-10-12 14:36:11 -0600

    Adding postgresql to our compose file

--- db.py
+++ db.py
@@ -11,7 +11,7 @@ load_dotenv()
 POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "123456")
 POSTGRES_DB = os.getenv("POSTGRES_DB", "OlimpoDB")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")  # API corre local
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "postgres")  # API corre local
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 
 # Connection string PostgreSQL
